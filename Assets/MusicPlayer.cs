6bf5c289a9b7e0bd55affd97e1e8876f675497b7
Josh Landron <jlandron@uw.edu>
2019-07-11 15:26:20 -0700

    Initial commit after first day

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake( ) {
+        DontDestroyOnLoad( this.gameObject );
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke( "loadFirstScene", 5f );
+    }
+
+    void loadFirstScene( ) {
+        SceneManager.LoadScene( 1 );
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    [Tooltip("In m*s^-1")][SerializeField] float xSpeed = 20;
+    [Tooltip( "In m*s^-1" )] [SerializeField] float ySpeed = 20;
+    [SerializeField] float xRange = 6f;
+    [SerializeField] float yRange = 4f;
+    
+
+    [SerializeField] float pitchFactor = -5;
+    [SerializeField] float controlPitchFactor = -10;
+    [SerializeField] float yawFactor = 5;
+    [SerializeField] float rollFactor = -15;
+
+    float horizontalThrow;
+    float verticalThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProccessTranlation( );
+        ProcessRotation( );
+        
+    }
+    
+    private void ProccessTranlation( ) {
+        this.horizontalThrow = CrossPlatformInputManager.GetAxis( "Horizontal" );
+        float xOffset = horizontalThrow * xSpeed * Time.deltaTime;
+        float newXPos = Mathf.Clamp( transform.localPosition.x + xOffset, -xRange, xRange );
+
+        this.verticalThrow = CrossPlatformInputManager.GetAxis( "Vertical" );
+        float yOffset = verticalThrow * ySpeed * Time.deltaTime;
+        float newZPos = Mathf.Clamp( transform.localPosition.y + yOffset, -yRange, yRange + 1.5f );
+
+        transform.localPosition = new Vector3( newXPos, newZPos, transform.localPosition.z );
+    }
+    private void ProcessRotation( ) {
+        float pitch = (transform.localPosition.y * pitchFactor) + (controlPitchFactor * this.verticalThrow);
+        float yaw = (transform.localPosition.x * yawFactor) + (yawFactor * horizontalThrow);
+        float roll = (rollFactor * horizontalThrow);
+        transform.localRotation = Quaternion.Euler( pitch, yaw, roll );
+    }
+}
